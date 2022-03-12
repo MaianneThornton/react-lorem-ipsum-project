149d2f6a08a0bd332386c9f3a67c6c17f0ea8900
Maianne Thornton <maianne.thornton@gmail.com>
2022-03-12 13:08:21 -0500

    feat: added title, form, and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  // Variable for the amount of paragraphs (entered by user)
+  const [count, setCount] = useState(0);
+  // Variable for the array of text to be displayed
+  const [text, setText] = useState([]);
+  // variable that will display hello world in the console once the lorem form is submitted (error handling)
+  const handleSubmit = 
+  (e) => {
+    e.preventDefault();
+    console.log(`hello world!`);
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
+  <section className="section-center">
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className="lorem-form" onSubmit={handleSubmit}>
+      <label htmlFor="amount">
+        paragraphs:
+      </label>
+      <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)} />
+      <button type="submit" className="btn">generate</button>
+    </form>
+
+  </section>
     )
 }
 
