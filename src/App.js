f590bcfc974f0bb8aad2d5c2c605906cb26a24fd
Maianne Thornton <maianne.thornton@gmail.com>
2022-03-12 13:43:30 -0500

    style: Formatted code. Initial version of project complete.

--- src/App.js
+++ src/App.js
@@ -6,49 +6,49 @@ function App() {
   // Variable for the array of text to be displayed
   const [text, setText] = useState([]);
   // variable that will display hello world in the console once the lorem form is submitted (error handling)
-  const handleSubmit = 
-  (e) => {
-    e.preventDefault();
-    console.log(typeof count);
-    // turns the string returned from the count into a number (error handling).
-    let amount = parseInt(count);
-    console.log(amount);
-    // Fixing the bug where the display would display all 8 if user entered a negative number
-    if(count <= 0){
-      amount = 1
-    }
-    if (count > 8){
-      amount = 8
-    }
+  const handleSubmit =
+    (e) => {
+      e.preventDefault();
+      console.log(typeof count);
+      // turns the string returned from the count into a number (error handling).
+      let amount = parseInt(count);
+      console.log(amount);
+      // Fixing the bug where the display would display all 8 if user entered a negative number
+      if (count <= 0) {
+        amount = 1
+      }
+      if (count > 8) {
+        amount = 8
+      }
 
-    // Displays the amount of paragraphs as selected by user (in the form).
-    setText(data.slice(0,amount))
-  }
+      // Displays the amount of paragraphs as selected by user (in the form).
+      setText(data.slice(0, amount))
+    }
   return (
     // Title
-  <section className="section-center">
-    <h3>tired of boring lorem ipsum?</h3>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
 
-    {/* Form */}
-    <form className="lorem-form" onSubmit={handleSubmit}>
-      <label htmlFor="amount">
-        paragraphs:
-      </label>
-      <input type="number" name="amount" id="amount" value={count} onChange={(e)=>setCount(e.target.value)} />
-      <button type="submit" className="btn">generate</button>
-    </form>
+      {/* Form */}
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type="submit" className="btn">generate</button>
+      </form>
 
-    {/* Text */}
-    <article className="lorem-text">
-      {/* Iterating over the data.js array and displaying every item in the array in paragraphs */}
-      {text.map((item,index) => {
-        return <p key={index}>{item}</p>
-      })}
-      
+      {/* Text */}
+      <article className="lorem-text">
+        {/* Iterating over the data.js array and displaying every item in the array in paragraphs */}
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
 
-    </article>
-  </section>
-    )
+
+      </article>
+    </section>
+  )
 }
 
 export default App;
